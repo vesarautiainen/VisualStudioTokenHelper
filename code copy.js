93c94a26d2cd388711b28531b275e8fe7c87970f
Vesa Rautiainen <vrautiainen@gmail.com>
2020-10-16 15:29:30 +0300

    Change the way the style nodes are collected

---
+++ code copy.js
@@ -0,0 +1,82 @@
+// This plugin will open a modal to prompt the user to enter a number, and
+// it will then create that many rectangles on the screen.
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs in the <script> tag inside "ui.html" which has a
+// full browser environment (see documentation).
+// This shows the HTML page in "ui.html".
+figma.showUI(__html__);
+var TokenType;
+(function (TokenType) {
+    TokenType[TokenType["Font"] = 1] = "Font";
+    TokenType[TokenType["Color"] = 2] = "Color";
+})(TokenType || (TokenType = {}));
+let nodesWithStyles = {};
+// Go through the whole subtree of item from the selection
+for (const node of figma.currentPage.selection) {
+    console.log(node);
+    if (node.type == "INSTANCE" || node.type == "GROUP" || node.type == "FRAME") {
+        var colorStyleArray = [];
+        var textStyleArray = [];
+        // Find all nodes with fill style and add to the array
+        const nodesWithColorStyle = node.findAll(n => n.type == "RECTANGLE" && n.fillStyleId !== figma.mixed ||
+            n.type == "INSTANCE" && n.fillStyleId !== "" ||
+            n.type == "TEXT" && n.fillStyleId !== "");
+        console.log("nodesWithColorStyle", nodesWithColorStyle);
+        nodesWithColorStyle.forEach(element => {
+            if (element.type == "RECTANGLE" || element.type == "TEXT" || element.type == "INSTANCE") {
+                colorStyleArray.push({ "nodeId": element.id, "value": element.name });
+            }
+        });
+        nodesWithStyles.ColorStyles = colorStyleArray;
+        // Find all nodes with text style and add to the array
+        const nodesWithTextStyle = node.findAll(n => n.type == "TEXT" && n.textStyleId !== "");
+        nodesWithTextStyle.forEach(element => {
+            if (element.type == "TEXT") {
+                const styleId = element.textStyleId;
+                if (styleId && styleId !== figma.mixed) {
+                    textStyleArray.push({ "nodeId": element.id, "value": figma.getStyleById(styleId).description });
+                }
+            }
+        });
+        nodesWithStyles.TextStyles = textStyleArray;
+        console.log("This is what I'm passing", nodesWithStyles);
+        // Handle the nodes with styles to the UI to show
+        figma.ui.postMessage(nodesWithStyles);
+        // for (const child of node.children) {
+        //   if (child.type == "RECTANGLE") {
+        //     let styleId = child.fillStyleId
+        //     if (styleId !== figma.mixed) {
+        //       console.log(figma.getStyleById(styleId).description)
+        //       figma.ui.postMessage(figma.getStyleById(styleId).description)
+        //     }
+        //   }
+        // } 
+    }
+}
+function createAnnotation(type) {
+    var component = figma.currentPage.findOne(n => n.name === "Token Annotation / Typography");
+    if (component != undefined) {
+        var instance = component.createInstance();
+        instance.name = "Font annotation";
+        return instance;
+    }
+}
+figma.ui.onmessage = msg => {
+    if (msg.type === 'create-typography-annotation-all') {
+        const nodes = [];
+        const annotation = createAnnotation(TokenType.Font);
+        if (annotation) {
+            figma.currentPage.appendChild(annotation);
+            nodes.push(annotation);
+            figma.currentPage.selection = nodes;
+            figma.viewport.scrollAndZoomIntoView(nodes);
+        }
+        else {
+            // @TODO: create a fallback annotation style
+            console.log("Error: No annotation instance found");
+        }
+    }
+    // Make sure to close the plugin when you're done. Otherwise the plugin will
+    // keep running, which shows the cancel button at the bottom of the screen.
+    //figma.closePlugin();
+};
--- code.ts
+++ code.ts
@@ -8,85 +8,83 @@
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-enum TokenType {
+enum TokenType2 {
   Font = 1,
   Color
 }
 
+let nodesWithStyles2: NodesWithStyles2 = {};
+var colorStyleArray = [];
+var textStyleArray = [];
 
-// @TODO: When you find an item with a text style or color style add it to the structure 
-// (add reference to the corresponding item so you can find it easily with that)
-
-// @TODO: Pass the structure to the UI layer and show the structure to the user
-
-// @TODO: Hover over a token item and show it on the page
-
-// @TODO: Button to create a token for an item
-
-// @TODO: Button to create all typography tokens
-
-// @TODO: Return the same structure filtered to items the user wants to add
-
-// @TODO: Create annotation instances of the selected tokens, inject correct data
-
-// @TODO: Place the annotation instances to the correct location
-
-interface NodesWithStyles {
+interface NodesWithStyles2 {
   ColorStyles?: any,
   TextStyles?: any
 }
 
-let nodesWithStyles: NodesWithStyles = {};
-// Go through the whole subtree of item from the selection
-for (const node of figma.currentPage.selection) {
-  console.log(node)
-  if (node.type == "INSTANCE" || node.type == "GROUP" || node.type == "FRAME") {
-    var colorStyleArray = [];
-    var textStyleArray = [];
-    // Find all nodes with fill style and add to the array
-    const nodesWithColorStyle = node.findAll(n => 
-      n.type == "RECTANGLE" && n.fillStyleId !== figma.mixed ||
-      n.type == "TEXT" && n.fillStyleId !== ""
-      );
-      console.log("nodesWithColorStyle", nodesWithColorStyle)
-      nodesWithColorStyle.forEach(element => {
-        if (element.type == "RECTANGLE" || element.type == "TEXT") {
-          colorStyleArray.push({"nodeId": element.id, "value": element.name})
-        }
-      });
-      nodesWithStyles.ColorStyles = colorStyleArray;
-
-    // Find all nodes with text style and add to the array
-    const nodesWithTextStyle = node.findAll(n => n.type == "TEXT" && n.textStyleId !== "");
-
-    nodesWithTextStyle.forEach(element => {
-      if (element.type == "TEXT") {
-        const styleId = element.textStyleId
-        if (styleId && styleId !== figma.mixed) {
-          textStyleArray.push({"nodeId": element.id, "value": figma.getStyleById(styleId).description})
-        }
-      }
-    });
-
-    nodesWithStyles.TextStyles = textStyleArray;
-    console.log("This is what I'm passing", nodesWithStyles)
-    // Handle the nodes with styles to the UI to show
-    figma.ui.postMessage(nodesWithStyles);
-
-    // for (const child of node.children) {
-    //   if (child.type == "RECTANGLE") {
-    //     let styleId = child.fillStyleId
-    //     if (styleId !== figma.mixed) {
-    //       console.log(figma.getStyleById(styleId).description)
-    //       figma.ui.postMessage(figma.getStyleById(styleId).description)
-    //     }
-    //   }
-    // } 
+function checkNodeForStyles(node) {
+  // Fill style. The token name is currently embedded in the node name.
+  if (node.fillStyleId != undefined && node.fillStyleId != "") {
+    colorStyleArray.push({"nodeId": node.id, "value": extractTokenNameFromNodeName(node.name)})
+  }
+    // Text style. The font token name is currently in the style description.
+  if (node.textStyleId != undefined && node.textStyleId != "") {
+    textStyleArray.push({"nodeId": node.id, "value": figma.getStyleById(node.textStyleId).description})
   }
 }
 
+function extractTokenNameFromNodeName(text: string):string {
+  let colorTokenId = "[token:";
+  let tokenValue = text;
+  let verifyString = text.toLowerCase();
+  let foundIndex = verifyString.indexOf(colorTokenId);
 
-function createAnnotation(type: TokenType):InstanceNode {
+  if (verifyString.includes(colorTokenId)) {
+    tokenValue = tokenValue.slice(foundIndex + colorTokenId.length)
+    tokenValue = tokenValue.replace(']', '');
+    tokenValue = tokenValue.replace(' ', '');
+  } else {
+    tokenValue = "No token id found"
+  }
+  return tokenValue;
+}
+
+function traverse(node) {
+  checkNodeForStyles(node)
+  if ("children" in node) {
+    for (const child of node.children) {
+      traverse(child)    
+    }
+  }
+}
+
+// START
+
+// Loading fonts
+figma.loadFontAsync({family: "Consolas", style: "Regular"}).then(initSuccess, initFailure)
+
+function initSuccess() {
+
+  // Go through the all the nodes in the current selection
+  for (const node of figma.currentPage.selection) {
+    traverse(node) 
+  }
+
+  nodesWithStyles2.ColorStyles = colorStyleArray;
+  nodesWithStyles2.TextStyles = textStyleArray;
+
+  // Send nodes with styles to the UI
+  figma.ui.postMessage(nodesWithStyles2);
+}
+
+function initFailure() {
+  console.log("Required fonts failed to load. Exit.")
+}
+
+
+
+// -------------------Events from the UI ------------------------------
+function createAnnotation2(type: TokenType2):InstanceNode {
   var component = <ComponentNode>figma.currentPage.findOne(n => n.name === "Token Annotation / Typography")
   if (component != undefined) {
     var instance:InstanceNode = component.createInstance();
@@ -100,19 +98,27 @@ figma.ui.onmessage = msg => {
   if (msg.type === 'create-typography-annotation-all') {
     const nodes: SceneNode[] = [];
 
-    const annotation = createAnnotation(TokenType.Font);
-    if (annotation) {
-      figma.currentPage.appendChild(annotation);
-      nodes.push(annotation);
-      figma.currentPage.selection = nodes;
-      figma.viewport.scrollAndZoomIntoView(nodes);
-    } else {
-      // @TODO: create a fallback annotation style
-      console.log("Error: No annotation instance found")
-    }
+    nodesWithStyles2.TextStyles.forEach(element => {
+      const annotationInstance = createAnnotation2(TokenType2.Font);
+      if (annotationInstance) {
+        // set data
+        var label = <TextNode>annotationInstance.findOne(n => n.type === "TEXT")
+        label.characters = element.value
 
+        figma.currentPage.appendChild(annotationInstance);
+        nodes.push(annotationInstance);
+      } else {
+        // @TODO: create a fallback annotation style
+        console.log("Error: No annotation instance found")
+      }
+    });
+    
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+    
   }
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
-  //figma.closePlugin();
-};
+
+}
+  
+// start the traversal at the root
+//figma.closePlugin()
