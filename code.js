902468da33694491a48317b115760693f32da892
Vesa Rautiainen <vrautiainen@gmail.com>
2020-10-22 11:30:31 +0300

    UI Changes

--- code.js
+++ code.js
@@ -5,12 +5,18 @@
 // full browser environment (see documentation).
 // This shows the HTML page in "ui.html".
 figma.showUI(__html__);
-var TokenType2;
-(function (TokenType2) {
-    TokenType2[TokenType2["Font"] = 1] = "Font";
-    TokenType2[TokenType2["Color"] = 2] = "Color";
-})(TokenType2 || (TokenType2 = {}));
-let nodesWithStyles2 = {};
+var TokenType;
+(function (TokenType) {
+    TokenType[TokenType["Font"] = 1] = "Font";
+    TokenType[TokenType["Color"] = 2] = "Color";
+})(TokenType || (TokenType = {}));
+var ColorTheme;
+(function (ColorTheme) {
+    ColorTheme[ColorTheme["Blue"] = 1] = "Blue";
+    ColorTheme[ColorTheme["Light"] = 2] = "Light";
+    ColorTheme[ColorTheme["Dark"] = 3] = "Dark";
+})(ColorTheme || (ColorTheme = {}));
+let nodesWithStyles = {};
 var colorStyleArray = [];
 var textStyleArray = [];
 function checkNodeForStyles(node) {
@@ -54,19 +60,19 @@ function initSuccess() {
     for (const node of figma.currentPage.selection) {
         traverse(node);
     }
-    nodesWithStyles2.ColorStyles = colorStyleArray;
-    nodesWithStyles2.TextStyles = textStyleArray;
+    nodesWithStyles.ColorStyles = colorStyleArray;
+    nodesWithStyles.TextStyles = textStyleArray;
     // Send nodes with styles to the UI
-    figma.ui.postMessage(nodesWithStyles2);
+    figma.ui.postMessage(nodesWithStyles);
 }
 function initFailure() {
     console.log("Required fonts failed to load. Exit.");
 }
 // -------------------Events from the UI ------------------------------
-function createAnnotation2(type) {
+function createAnnotation(type) {
     var component;
     var instanceName;
-    if (type == TokenType2.Font) {
+    if (type == TokenType.Font) {
         component = figma.currentPage.findOne(n => n.name === "Token Annotation / Typography");
         instanceName = "Font Annotation";
     }
@@ -83,63 +89,97 @@ function createAnnotation2(type) {
         console.log("Error: Could not creat annotation instance");
     }
 }
+//Commands from the UI 
 figma.ui.onmessage = msg => {
     if (msg.type === 'create-color-annotation-all') {
-        const nodes = [];
-        nodesWithStyles2.ColorStyles.forEach(element => {
-            const annotationInstance = createAnnotation2(TokenType2.Color);
-            if (annotationInstance) {
-                // set data
-                var label = annotationInstance.findOne(n => n.type === "TEXT");
-                label.characters = "Color Token: " + element.value;
-                // find the original node and set the annotation label position
-                let originalNode = figma.getNodeById(element.nodeId);
-                console.log(originalNode.absoluteTransform);
-                let xOrigin = originalNode.absoluteTransform[0][2];
-                let yOrigin = originalNode.absoluteTransform[1][2];
-                annotationInstance.resize(annotationInstance.width, randomInteger(30, 150));
-                let xPos = xOrigin - annotationInstance.width / 2 + originalNode.width / 2;
-                let yPos = yOrigin - annotationInstance.height + originalNode.height / 2;
-                annotationInstance.relativeTransform = [[1, 0, xPos], [0, 1, yPos]];
-                figma.currentPage.appendChild(annotationInstance);
-                nodes.push(annotationInstance);
-            }
-            else {
-                // @TODO: create a fallback annotation style
-                console.log("Error: No annotation instance found");
-            }
-        });
+        annotateAllColorTokens();
     }
-    if (msg.type === 'create-typography-annotation-all') {
-        const nodes = [];
-        nodesWithStyles2.TextStyles.forEach(element => {
-            const annotationInstance = createAnnotation2(TokenType2.Font);
-            if (annotationInstance) {
-                // set data
-                var label = annotationInstance.findOne(n => n.type === "TEXT");
-                label.characters = "Font Token: " + element.value;
-                // find the original node and set the annotation label position
-                let originalNode = figma.getNodeById(element.nodeId);
-                console.log(originalNode.absoluteTransform);
-                let xOrigin = originalNode.absoluteTransform[0][2];
-                let yOrigin = originalNode.absoluteTransform[1][2];
-                let xPos = xOrigin - annotationInstance.width / 2 + originalNode.width / 2;
-                let yPos = yOrigin + originalNode.height / 2;
-                annotationInstance.relativeTransform = [[1, 0, xPos], [0, 1, yPos]];
-                figma.currentPage.appendChild(annotationInstance);
-                nodes.push(annotationInstance);
-            }
-            else {
-                // @TODO: create a fallback annotation style
-                console.log("Error: No annotation instance found");
-            }
-        });
-        figma.currentPage.selection = nodes;
-        figma.viewport.scrollAndZoomIntoView(nodes);
+    else if (msg.type === 'create-typography-annotation-all') {
+        annotateAllTypographyTokens();
+    }
+    else if (msg.type === 'change-theme-version') {
+        // @TODO: figure out how to do this theming properly in type script.
+        if (msg.themeId == "Dark") {
+            changeTheme(ColorTheme.Dark);
+        }
     }
 };
+// Annotate tokens
+//------------------------------------------------------
+function annotateAllColorTokens() {
+    const nodes = [];
+    nodesWithStyles.ColorStyles.forEach(element => {
+        const annotationInstance = createAnnotation(TokenType.Color);
+        if (annotationInstance) {
+            // set data
+            var label = annotationInstance.findOne(n => n.type === "TEXT");
+            label.characters = "Color Token: " + element.value;
+            // find the original node and set the annotation label position
+            let originalNode = figma.getNodeById(element.nodeId);
+            console.log(originalNode.absoluteTransform);
+            let xOrigin = originalNode.absoluteTransform[0][2];
+            let yOrigin = originalNode.absoluteTransform[1][2];
+            annotationInstance.resize(annotationInstance.width, randomInteger(30, 150));
+            let xPos = xOrigin - annotationInstance.width / 2 + originalNode.width / 2;
+            let yPos = yOrigin - annotationInstance.height + originalNode.height / 2;
+            annotationInstance.relativeTransform = [[1, 0, xPos], [0, 1, yPos]];
+            figma.currentPage.appendChild(annotationInstance);
+            nodes.push(annotationInstance);
+        }
+        else {
+            // @TODO: create a fallback annotation style
+            console.log("Error: No annotation instance found");
+        }
+    });
+}
+function annotateAllTypographyTokens() {
+    const nodes = [];
+    nodesWithStyles.TextStyles.forEach(element => {
+        const annotationInstance = createAnnotation(TokenType.Font);
+        if (annotationInstance) {
+            // set data
+            var label = annotationInstance.findOne(n => n.type === "TEXT");
+            label.characters = "Font Token: " + element.value;
+            // find the original node and set the annotation label position
+            let originalNode = figma.getNodeById(element.nodeId);
+            console.log(originalNode.absoluteTransform);
+            let xOrigin = originalNode.absoluteTransform[0][2];
+            let yOrigin = originalNode.absoluteTransform[1][2];
+            let xPos = xOrigin - annotationInstance.width / 2 + originalNode.width / 2;
+            let yPos = yOrigin + originalNode.height / 2;
+            annotationInstance.relativeTransform = [[1, 0, xPos], [0, 1, yPos]];
+            figma.currentPage.appendChild(annotationInstance);
+            nodes.push(annotationInstance);
+        }
+        else {
+            // @TODO: create a fallback annotation style
+            console.log("Error: No annotation instance found");
+        }
+    });
+    figma.currentPage.selection = nodes;
+    figma.viewport.scrollAndZoomIntoView(nodes);
+}
+// Change theme (placeholder functions)
+//------------------------------------------------------
+function changeTheme(theme) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            myFunction(this);
+        }
+    };
+    xhttp.open("GET", "Themes/Theme.Dark.xml", true);
+    xhttp.send();
+}
+function myFunction(xml) {
+    var xmlDoc = xml.responseXML;
+    //     var x = xmlDoc.getElementsByTagName('Category').[Name "Components"];
+    //     var y = x.childNodes[0];
+    //     document.getElementById("demo").innerHTML = y.nodeValue
+}
+// Utils 
+//------------------------------------------------------
 function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-// start the traversal at the root
 //figma.closePlugin()
